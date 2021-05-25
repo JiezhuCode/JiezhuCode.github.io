cc41ade0932410f2dd0a90a2574d2793d969f58e
JiezhuCode <62168752+JiezhuCode@users.noreply.github.com>
2021-05-25 20:18:32 +0800

    Site updated: 2021-05-25 20:18:32

--- js/src/FunnyTitle.js
+++ js/src/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next");
+         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
          document.title = 'ヾ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next");
+         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
          document.title = 'ヾ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
